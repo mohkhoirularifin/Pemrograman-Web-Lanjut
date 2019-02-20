134b5b491fe52723856d7adb20b7a40197f1e165
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-02-20 18:04:05 +0700

    Initial commit

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  tittle = 'Belajar Angular';
+  Courses;
+
+  binding = 'property-binding';
+  imageUrl = 'http://lorempixel.com/400/200';
+  // percobaan = 'ini percobaan';
+  // colSpan = 2;
+  // isActive=true;
+  // nama = 'Khoirul';
+
+  // onSave($event){
+  //   $event.stopPropagation();
+  //   console.log("onSave Sudah diKlick", $event);
+  // }
+
+  // onDivClick($event){
+  //   console.log("onDivClick Sudah diKlick", $event);
+  // }
+
+  // onKeyUp(){
+  //   console.log(this.nama);
+  // }
+  constructor(private service:CoursesService) { 
+    this.Courses=service.getCourses();
+  }
+
+  ngOnInit() {
+  }
+
+}
