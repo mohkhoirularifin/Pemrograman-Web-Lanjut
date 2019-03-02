c1c4290c989e1caeca4239cf4b84872d9c57f07b
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-02 12:19:44 +0700

    Update app.component.ts

--- Praktikum5/src/app/app.component.ts
+++ Praktikum5/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Praktikum 4';
+  title = 'belajar-angular';
+
+  post={
+    titles:"ini post",
+    isFavorite:true
+  }
 }
