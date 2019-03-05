826aca279d479fde371211c801d0aeea41ac5bb1
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-05 22:10:20 +0700

    Update favorite.component.ts

--- Praktikum5/src/app/favorite/favorite.component.ts
+++ Praktikum5/src/app/favorite/favorite.component.ts
@@ -2,10 +2,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
-  // templateUrl: './favorite.component.html',
-  template: `<h2> ini menggunakan internal template <h2>`,
+  templateUrl: './favorite.component.html',
+  // template: `<h2> ini menggunakan internal template <h2>`,
   styleUrls: ['./favorite.component.css'],
   // inputs: ['isFavorite']
+  styles:[
+    `
+    .fa-star{
+      color:green;
+    }
+    `
+  ]
 })
 export class FavoriteComponent implements OnInit {
 
