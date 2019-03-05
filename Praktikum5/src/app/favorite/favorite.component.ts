14f28244bf9ad1b68f8736c957a3c17de3b22ce9
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-05 20:27:43 +0700

    Update favorite.component.ts

--- Praktikum5/src/app/favorite/favorite.component.ts
+++ Praktikum5/src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
+  @Input() isFavorite:boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+    this.isFavorite=!this.isFavorite;
   }
 
 }
