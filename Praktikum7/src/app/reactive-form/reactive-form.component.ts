745cc1fbfd33e90c91ab6d337364da7c2efda16c
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-20 15:02:09 +0700

    Update reactive-form.component.ts

--- Praktikum7/src/app/reactive-form/reactive-form.component.ts
+++ Praktikum7/src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,8 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { mahasiswa } from './../mahasiswa.interface';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  mahasiswa:FormGroup;
+
+  constructor(private mhs:FormBuilder) 
+  { 
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  // method untuk generate form
+  createForm(){
+    this.mahasiswa = this.mhs.group({
+      nama_mhs:'',
+      pendidikan:this.mhs.group({
+        nama_jurusan:'',
+        nama_prodi:''
+      })
+    })
+  }
+
+  onSubmit(f:FormGroup){
+    console.log("hasil dari inputan adalah");
+    console.log(f.value);
+  }
 }
