c4f03082df4cf377b4678f2c5442bffa7a554b23
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-20 15:54:44 +0700

    Update app.module.ts

--- Praktikum7/src/app/app.module.ts
+++ Praktikum7/src/app/app.module.ts
@@ -4,24 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses/courses.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { InputFormatDirective } from './input-format.directive';
-import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    ServerComponent,
-    ServersComponent,
-    FavoriteComponent,
-    InputFormatDirective,
-    ContactComponent,
     ReactiveFormComponent,
     TemplateDrivenComponent
   ],
