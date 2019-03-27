97e706580a125dc12eca08e772b2cb307200bd16
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-03-27 17:21:57 +0700

    Update signup-form.component.ts

--- Praktikum8/src/app/signup-form/signup-form.component.ts
+++ Praktikum8/src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'app-signup-form',
@@ -10,7 +11,8 @@ export class SignupFormComponent implements OnInit {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(5)
+      Validators.minLength(5),
+      UsernameValidators.cannotContainSpace
     ]),
     password: new FormControl('', Validators.required)
   })
