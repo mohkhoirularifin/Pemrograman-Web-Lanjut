df9688e4c096bcb6b548f7de88bcce1942b6f18f
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-04-24 14:31:43 +0700

    Update app.module.ts

--- Praktikum9/src/app/app.module.ts
+++ Praktikum9/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
