37f7c5888cced6dca5b845e41d11a396641e6056
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-05-08 16:50:59 +0700

    Update fake-backend.ts

--- auth-demo-starter/auth-demo/src/app/helpers/fake-backend.ts
+++ auth-demo-starter/auth-demo/src/app/helpers/fake-backend.ts
@@ -5,7 +5,7 @@ export function fakeBackendFactory(
     backend: MockBackend, 
     options: BaseRequestOptions) {
         
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNzQxNzIwMDUxIiwibmFtZSI6Ik1vaCBLaG9pcnVsIEFyaWZpbiIsImFkbWluIjp0cnVlfQ.2KVAU31v1mdqqrbztmL1HbZRy2npO9sHbUMlRJmAlnQ';
     
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an 
