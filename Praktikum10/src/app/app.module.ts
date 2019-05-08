cb7f122767b289571ee028511052f5fab8f18f57
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-05-08 12:32:39 +0700

    Update app.module.ts

--- Praktikum10/src/app/app.module.ts
+++ Praktikum10/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,24 +12,28 @@ import { FormMemberComponent } from './form-member/form-member.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // TemplateDrivenComponent,
-    // ContactComponent,
-    // SignupFormComponent,
-    PostsComponent,
     FormMemberComponent,
+    HomeComponent,
     NavbarComponent,
-    NotFoundComponent,
-    HomeComponent
+    PostsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'form', component:FormMemberComponent},
+      {path:'post', component:PostsComponent},
+      {path:'**', component:NotFoundComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
